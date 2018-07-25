eb7600c95c5a97a32529cf918adac9fa2cf5b968
Brian Speagle <bspeagle@gmail.com>
2018-07-25 16:45:55 -0400

    docker jenkins!

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -4,13 +4,24 @@ node {
         git url: 'https://github.com/bspeagle/PeopleFinder.git'
     }
 
-    stage('Terraform!') {
+    stage('Build Docker Image') {
+        sh '$(aws ecr get-login --no-include-email --region us-east-1)'
+        sh 'docker build . -t peopleFinder'
+        sh 'docker tag peoplefinder:latest 367592122643.dkr.ecr.us-east-1.amazonaws.com/peoplefinder:latest'
+    }
+
+    stage('Push Docker Image to ECR') {
+        sh 'docker push 367592122643.dkr.ecr.us-east-1.amazonaws.com/peoplefinder:latest'
+    }
+
+    /*stage('Terraform!') {
         dir ('terraform') {
             sh 'terraform init'
             configFileProvider([configFile(fileId: 'LMB-TF-VARS', targetLocation: './files/')]) {
-                //sh 'terraform destroy -auto-approve -var-file="./files/terraform.tfvars"'
                 sh 'terraform apply -auto-approve -var-file="./files/terraform.tfvars"'
             }
         }
-    }
+    }*/
+
+    
 }

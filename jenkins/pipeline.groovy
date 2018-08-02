ecdc06c216af35df09faca64ed326b64665d1561
Brian Speagle <bspeagle@gmail.com>
2018-08-02 13:45:45 -0400

    destroy!

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -33,6 +33,7 @@ node {
             }
             sh 'terraform init'
             configFileProvider([configFile(fileId: 'LMB-TF-VARS', targetLocation: '../../files/')]) {
+                sh 'terraform destroy -auto-approve -var-file="../../files/terraform.tfvars"'
                 sh 'terraform apply -auto-approve -var-file="../../files/terraform.tfvars"'
             }
             echo 'Uploading .tfstate to S3.'

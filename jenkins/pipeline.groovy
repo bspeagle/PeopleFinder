d431180cc07690695b9ec6e41a18f08761923ccf
Brian Speagle <bspeagle@gmail.com>
2018-08-03 21:03:11 -0400

    m

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -34,7 +34,7 @@ node {
             sh 'terraform init'
             configFileProvider([configFile(fileId: 'LMB-TF-VARS', targetLocation: '../../files/')]) {
                 sh 'terraform destroy -auto-approve -var-file="../../files/terraform.tfvars"'
-                //sh 'terraform apply -auto-approve -var-file="../../files/terraform.tfvars"'
+                sh 'terraform apply -auto-approve -var-file="../../files/terraform.tfvars"'
             }
             echo 'Uploading .tfstate to S3.'
             withCredentials([usernamePassword(credentialsId: 'peopleFinder-S3', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {

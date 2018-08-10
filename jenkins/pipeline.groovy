3962f1725114357fd7e4d1b5a7e68bdee94f9083
Brian Speagle <bspeagle@gmail.com>
2018-08-10 10:10:48 -0400

    another change

--- jenkins/pipeline.groovy
+++ jenkins/pipeline.groovy
@@ -34,11 +34,19 @@ node {
             sh 'terraform init'
             configFileProvider([configFile(fileId: 'LMB-TF-VARS', targetLocation: '../files/')]) {
                 sh 'terraform destroy -auto-approve -var-file="../files/terraform.tfvars"'
-                sh 'terraform apply -auto-approve -var-file="../files/terraform.tfvars"'
-            }
-            echo 'Uploading .tfstate to S3.'
-            withCredentials([usernamePassword(credentialsId: 'peopleFinder-S3', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-                sh 'AWS_ACCESS_KEY_ID=$USERNAME AWS_SECRET_ACCESS_KEY=$PASSWORD aws s3 cp ./terraform.tfstate s3://peoplefinder-files/terraform/prod/'
+                try {
+                    sh 'terraform apply -auto-approve -var-file="../files/terraform.tfvars"'
+                    echo 'Uploading .tfstate to S3.'
+                    withCredentials([usernamePassword(credentialsId: 'peopleFinder-S3', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                        sh 'AWS_ACCESS_KEY_ID=$USERNAME AWS_SECRET_ACCESS_KEY=$PASSWORD aws s3 cp ./terraform.tfstate s3://peoplefinder-files/terraform/prod/'
+                    }
+                }
+                catch(Exception ex) {
+                    echo 'Terraform apply failed :('
+                    echo 'Uploading .tfstate to S3.'
+                    withCredentials([usernamePassword(credentialsId: 'peopleFinder-S3', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                        sh 'AWS_ACCESS_KEY_ID=$USERNAME AWS_SECRET_ACCESS_KEY=$PASSWORD aws s3 cp ./terraform.tfstate s3://peoplefinder-files/terraform/prod/'
+                }
             }
         }
     }

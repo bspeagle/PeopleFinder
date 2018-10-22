b12c9404020aeb6b83eeda37928a0e4dd5f11cb4
Brian Speagle <bspeagle@gmail.com>
2018-10-22 12:59:51 -0400

    4

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -3,8 +3,8 @@ node {
         sh 'rm -r -f PeopleFinder'
         sh 'git clone https://github.com/bspeagle/PeopleFinder.git'
         dir ('PeopleFinder') {
-            configFileProvider([configFile(fileId: 'peopleFinder_ENV', targetLocation: '.env')]) {
-                configFileProvider([configFile(fileId: 'bspeagle_PEM', targetLocatiopn: 'bspeagle.pem')]) {
+            configFileProvider([configFile(fileId: 'peopleFinder_ENV', targetLocation: '.')]) {
+                configFileProvider([configFile(fileId: 'bspeagle_PEM', targetLocatiopn: '.')]) {
                     sh 'ls'
                     sh 'scp -i "bspeagle.pem" ../PeopleFinder ec2-user@34.201.20.194:/home/ec2-user'
                 }

fff9f42f7307723680bc5c2ea8cb26786614d424
Brian Speagle <bspeagle@gmail.com>
2018-10-22 12:50:45 -0400

    updated pipeline for .env deployment

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -1,8 +1,19 @@
 node {
+    stage("Clone repo and add .env file") {
+        sh 'rm -r -f PeopleFinder'
+        sh 'git clone https://github.com/bspeagle/PeopleFinder.git'
+        dir ('PeopleFinder') {
+            configFileProvider([configFile(fileId: 'peopleFinder_ENV', targetLocation: '.')]) {
+                configFileProvider([configFile(fileId: 'bspeagle_PEM', targetLocatiopn: '.')]) {
+                    sh 'scp -i "bspeagle.pem" ../PeopleFinder ec2-user@34.201.20.194:/home/ec2-user'
+                }
+            }
+        }
+    }
     stage("SSH t-t-time!") {
         sshagent (credentials: ['aws-ec2-user']) {
             withCredentials([string(credentialsId: 'appServer', variable: 'IP')]) {
-                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "rm -r -f PeopleFinder; git clone https://github.com/bspeagle/PeopleFinder.git; cd PeopleFinder; composer install --no-interaction; cd ..; rsync -av --progress PeopleFinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; rm -r PeopleFinder"'
+                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "cd PeopleFinder; composer install --no-interaction; cd ..; rsync -av --progress PeopleFinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; rm -r PeopleFinder"'
             }
         }
     }

d75c4e67d027c75bd414c64cd6228c56d97ef88b
Brian Speagle <bspeagle@gmail.com>
2018-10-22 13:24:25 -0400

    17

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -9,7 +9,7 @@ node {
         }
         configFileProvider([configFile(fileId: 'bspeagle_PEM', targetLocation: '.')]) {
             sh 'ls -a -l'
-            sh 'sudo su scp -i bspeagle.pem PeopleFinder ec2-user@34.201.20.194:/home/ec2-user'
+            sh 'scp -i bspeagle.pem PeopleFinder ec2-user@34.201.20.194:/home/ec2-user'
         }
     }
     stage("SSH t-t-time!") {

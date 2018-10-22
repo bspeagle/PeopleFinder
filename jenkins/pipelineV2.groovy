fd1035502a8ef6b99fc633dc337a213ab3b56784
Brian Speagle <bspeagle@gmail.com>
2018-10-22 13:12:16 -0400

    10

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -5,7 +5,6 @@ node {
         dir ('PeopleFinder') {
             configFileProvider([configFile(fileId: 'peopleFinder_ENV', targetLocation: '.')]) {
                 sh 'mv pf.env .env'
-                sh 'ls -a'
                 sh 'scp -i ../bspeagle.pem ../PeopleFinder ec2-user@34.201.20.194:/home/ec2-user'
             }
         }

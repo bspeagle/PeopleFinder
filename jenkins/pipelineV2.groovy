ba8c0d3f6751c281ed424bb2c26e61245f3a5b23
Brian Speagle <bspeagle@gmail.com>
2018-12-17 21:13:55 -0500

    pipeline change

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -2,7 +2,7 @@ node {
     stage("SSH t-t-time!") {
         sshagent (credentials: ['aws-ec2-user']) {
             withCredentials([string(credentialsId: 'appServer', variable: 'IP')]) {
-                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "rm -r -f PeopleFinder; git clone https://github.com/bspeagle/peoplefinder.git; cd PeopleFinder; composer install --no-interaction; cd ..; rsync -av --progress PeopleFinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; cd ..; rm -r PeopleFinder"'
+                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "rm -r -f peoplefinder; git clone https://github.com/bspeagle/peoplefinder.git; cd peoplefinder; composer install --no-interaction; cd ..; rsync -av --progress PeopleFinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; cd ..; rm -r peoplefinder"'
             }
         }
     }

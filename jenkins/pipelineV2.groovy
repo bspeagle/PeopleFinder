f7502bd975aed9179d0584229160cc88202cbbbe
Brian Speagle <bspeagle@gmail.com>
2018-12-17 21:21:23 -0500

    s3 copy addition

--- jenkins/pipelineV2.groovy
+++ jenkins/pipelineV2.groovy
@@ -2,7 +2,7 @@ node {
     stage("SSH t-t-time!") {
         sshagent (credentials: ['aws-ec2-user']) {
             withCredentials([string(credentialsId: 'appServer', variable: 'IP')]) {
-                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "rm -r -f peoplefinder; git clone https://github.com/bspeagle/peoplefinder.git; cd peoplefinder; composer install --no-interaction; cd ..; rsync -av --progress peoplefinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; rm -r peoplefinder"'
+                sh 'ssh -o StrictHostKeyChecking=no -l ec2-user $IP "rm -r -f peoplefinder; git clone https://github.com/bspeagle/peoplefinder.git; cd peoplefinder; composer install --no-interaction; cd ..; rsync -av --progress peoplefinder /var/www/html --exclude terraform --exclude jenkins --exclude .git --exclude .vscode; rm -r peoplefinder; aws s3 cp s3://peoplefinder-prod-filez/.env /var/www/html/peoplefinder;"'
             }
         }
     }
